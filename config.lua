d7a0a8e2918853129507dd847370378bf79b810e
cmd <96431684+cmd2401@users.noreply.github.com>
2022-09-29 22:23:08 +0200

    config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+-- 
+Config.JobName = "sapd"
+
+-- Blip
+Config.BlipSprite = 126
+Config.BlipDisplay = 4
+Config.BlipScale = 1.3
+Config.BlipColor = 29
+
+
+
+Config.RemoveBlipTime = 500000
+
+-- Translation
+Config.Translation            = "Officer "
+Config.Translation2           = " need help"
+
